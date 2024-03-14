96425bf3b72cee48131eb2632cbc640ca5856eb5
pegaseius <57208275+pegaseius@users.noreply.github.com>
2024-03-14 17:31:37 +0800

    Update visitor_counter.js

--- visitor_counter.js
+++ visitor_counter.js
@@ -10,23 +10,11 @@ const timeDifference = currentTimeStamp - startTimeStamp;
 // 将时间差转换为秒数
 const timeDifferenceInSeconds = Math.floor(timeDifference / 1000);
 
-// 每隔10秒增加的访客数量范围
-const minIncrease = 1; // 最小增加数量
-const maxIncrease = 1; // 最大增加数量
-
-// 计算当前时间段内的访客增加数量
-const visitorIncrease = Math.floor(timeDifferenceInSeconds / 10) * getRandomInt(minIncrease, maxIncrease);
-
 // 基础访客数量
 const baseVisitorCount = 10075;
 
 // 计算当前访客数量
-const currentVisitorCount = baseVisitorCount + visitorIncrease;
+const currentVisitorCount = baseVisitorCount + timeDifferenceInSeconds;
 
-// 将当前访客数量显示在页面上
-document.getElementById('visitorCount').textContent = currentVisitorCount;
-
-// 获取指定范围内的随机整数
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// 返回当前访客数量
+export default currentVisitorCount;
